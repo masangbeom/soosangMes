95cef469bda318cdfedb20a2198b0d2257fb1025
sangbeom ma <aktkdqja12@gmail.com>
2017-07-17 10:57:00 +0900

    공장 및 라인선택 동적 디자인 및 뷰 완료.

--- src/pages/factory-monitoring/factory-monitoring.ts
+++ src/pages/factory-monitoring/factory-monitoring.ts
@@ -6,12 +6,66 @@ import { NavController, NavParams } from 'ionic-angular';
   templateUrl: 'factory-monitoring.html',
 })
 export class FactoryMonitoringPage {
+  private factories: any;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad FactoryMonitoringPage');
+    this.factories=[];
+    let lines =[];
+    let line1 = {
+      name: "Line 1",
+      lineId: 1
+    }
+    let line2 = {
+      name: "Line 2",
+      lineId: 2
+    }  
+    let line3 = {
+      name: "Line 3",
+      lineId: 3
+    }  
+    let line4 = {
+      name: "Line 4",
+      lineId: 4
+    }
+    lines.push(line1);
+    lines.push(line2);
+    lines.push(line3);    
+    lines.push(line4);
+
+    let factory1 = {
+      title: "Factory 1",
+      description: "공장 설명",
+      lines: lines
+    }
+    let factory2 = {
+      title: "Factory 2",
+      description: "공장 설명",
+      lines: lines
+    }
+    let factory3 = {
+      title: "Factory 3",
+      description: "공장 설명",
+      lines: lines
+    }
+    let factory4 = {
+      title: "Factory 4",
+      description: "공장 설명",
+      lines: lines
+    }
+    this.factories.push(factory1);
+    this.factories.push(factory2);
+    this.factories.push(factory3);
+    this.factories.push(factory4);
+    
+
+  }
+
+  viewMonitoring(lineId){
+    console.log(lineId);
   }
 
 }
